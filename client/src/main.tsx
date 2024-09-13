dbf8304076001c9339a8c2f2dc0745caf66316a4
Jiri Javora <javorji1@fit.cvut.cz>
2024-09-13 17:24:45 +0200

    chore: update deps

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,16 +3,16 @@ import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.bundle';
 
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 
 import App from './App';
 import { VictimProvider } from './contexts/VictimContext';
 
-ReactDOM.render(
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <VictimProvider>
       <App />
     </VictimProvider>
   </React.StrictMode>,
-  document.getElementById('root'),
 );
